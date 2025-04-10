61afb54a4c986eb5450e5e62a9f4760c95a1afe3
NimrodLobozar <nimrod.lobozar@gmail.com>
2025-04-10 10:16:00 +0200

    Refactor users table migration to include additional fields and constraints

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,11 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('name', 255)->unique();
+            $table->string('password', 255);
+            $table->string('first_name', 255)->nullable();
+            $table->string('middle_name', 100)->nullable();
+            $table->string('last_name', 255)->nullable();
+            $table->tinyInteger('role')->default(1);
+            $table->timestamp('logged_in_at')->nullable();
+            $table->string('token', 100)->nullable();
             $table->timestamps();
         });
 
