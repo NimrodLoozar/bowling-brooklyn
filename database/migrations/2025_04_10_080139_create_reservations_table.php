a5fe6c22b59de1def2d7ec821d0c8463316cb759
NimrodLobozar <nimrod.lobozar@gmail.com>
2025-04-10 10:11:55 +0200

    Remove redundant closing brackets in reservations table migration

--- database/migrations/2025_04_10_080139_create_reservations_table.php
+++ database/migrations/2025_04_10_080139_create_reservations_table.php
@@ -28,8 +28,6 @@ return new class extends Migration
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('baan_id')->references('id')->on('baan')->onDelete('cascade');
         });
-        });
-        });
     }
 
     /**
