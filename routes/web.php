c563cee63558fa2186eccbb903bea238b3ef14fa
Martijn339074 <339074@student.mboutrecht.nl>
2025-04-10 10:12:29 +0200

    Added contact route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ContactController;
 use App\Livewire\Settings\Appearance;
 use App\Livewire\Settings\Password;
 use App\Livewire\Settings\Profile;
@@ -20,5 +21,8 @@ Route::middleware(['auth'])->group(function () {
     Route::get('settings/password', Password::class)->name('settings.password');
     Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
 });
+
+// Contacts
+Route::resource('contacts', ContactController::class);
 
 require __DIR__.'/auth.php';
